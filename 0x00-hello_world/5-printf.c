14969f7212c9de8177e7d11214a787c9f297ff94
EkeminiDavid <ekeminismart8@gmail.com>
2023-09-12 07:33:13 -0700

    Hot fix

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - prints a statement
+ *
+ * Description: The function prints out with prop
+ * grammar, but the outcome is a piece of art
+ * Return: return 0 to end the program
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
